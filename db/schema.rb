f181b4ad41cbfe6eb0a66feb0851b8eb58764855
Alvin Voo <xoalvinox@gmail.com>
2019-01-29 23:56:40 +0800

    Added UserStocks table, routes, model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_075733) do
+ActiveRecord::Schema.define(version: 2019_01_29_151802) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_01_28_075733) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ticker"
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "stock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
